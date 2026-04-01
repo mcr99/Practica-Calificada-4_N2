47996a4cde1c4751744f0637e763684c990cf624
marvin <mcanastuj1999@gmail.com>
2026-04-01 13:51:56 -0600

    header resposive added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
-import { useState } from 'react'
+import { Route, Routes } from "react-router-dom"
+import Header from "./components/Header"
+import Home from "./pages/Home"
+
 
 function App() {
   return (
     <>
+    <Header/>
+    <Routes>
+    <Route path="/" element={<Home/>}></Route>
+    </Routes>
     </>
   )
 }
